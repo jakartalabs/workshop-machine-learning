50629629847527f16665fa066c029253984757de
satriawadhipurusa <satriawicaksanaadhipurusa@gmail.com>
2020-06-14 09:22:07 +0700

    reset boiler plate code

--- Part 8 - Create your own of Sentiment Classification/app/__init__.py
+++ Part 8 - Create your own of Sentiment Classification/app/__init__.py
@@ -1,29 +1,7 @@
-from flask import Flask, render_template, request
-import pickle
+from flask import Flask
 
 app = Flask(__name__)
 
-with open('model.pkl', 'rb') as file_model:
-    model = pickle.load(file_model)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def homepage():
-    if request.method == "POST":
-        kalimat = str(request.form['kalimat'])
-        pred = model.predict([kalimat])
-        if pred[0] == -1:
-            sentiment = 'negatif'
-        elif pred[0] == 0:
-            sentiment = 'netral'
-        elif pred[0] == 1:
-            sentiment = 'positif'
-        else:
-            sentiment = 'undefined'
-        return render_template("index.html", hasil_sentimen=sentiment)
-    return render_template("index.html")
-
 
 if __name__ == "__main__":
-    app.debug = True
     app.run()
